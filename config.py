e7c50a68358caf94d9b02c2207907147e7f920e3
Eshaan V Kirpal <eshaan90@gmail.com>
2024-04-06 19:14:43 +0530

    Added config file and argparser option

---
+++ config.py
@@ -0,0 +1,17 @@
+# Enter the site name which you want to block
+SITES_TO_BLOCK = [
+    "www.facebook.com",
+    "www.facebook.com/*"
+    "www.youtube.com",
+    "www.gmail.com",
+    "gmail.com",
+    "www.amazon.com",
+    "www.amazon.in",
+    "boardgamesindia.com",
+    "www.gameistry.in",
+    "www.flipkart.com"
+]
+
+START_HOUR=9
+END_HOUR=18
+
